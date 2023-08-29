9341058fd8a01be6c7aa9397cb546d6d22e535fa
Paola Martins <paola.nmartins@gmail.com>
2023-08-29 19:28:14 -0300

    sitemobapi

---
+++ src/main/java/br/com/mob/sitemobapi/SitemobApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mob.sitemobapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemobApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemobApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mob/sitemobapi/SitemobApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mob.sitemobapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemobApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
